227b8c3a02fbad6999e92e96dd675a841974b315
jamonroe <jasonmonroe90@gmail.com>
2014-04-10 17:44:02 -0700

    Use Test.java and inputjason.txt to see some results

--- frontend/jason/Test.java
+++ frontend/jason/Test.java
@@ -10,7 +10,7 @@ import frontend.token.jason.Token;
 public class Test {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Scanner scan = new Scanner(new Source(new FileInputStream(new File("input.txt"))));
+		Scanner scan = new Scanner(new Source(new FileInputStream(new File("inputjason.txt"))));
 		Token token;
 		while ((token = scan.next()) != null)
 			System.out.println(token);
